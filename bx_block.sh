19cebb9419f51dde9c12e1373aa1eb7bad532733
Slawka <slawka@eth0.org.ru>
2025-12-07 20:07:34 +0300

    I rewrote the blocking using the fail2ban
    I rewrote the blocking using the fail2ban

--- bx_block.sh
+++ bx_block.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Before Run
-# Add
-# iptables -N bx_block;
-# iptables -I INPUT -j bx_block
-
-
-ip_block=`mysql -s -e "select distinct concat('iptables -I bx_block -p tcp -m tcp --dport 443 -s ',REMOTE_ADDR,' -j DROP;') ip from (select AUDIT_TYPE_ID ,REMOTE_ADDR,COUNT(*) as cn  from sitemanager.b_event_log
+ip_block=`mysql -s -e "
+select REMOTE_ADDR ip from (select REMOTE_ADDR,COUNT(*) as cn  from sitemanager.b_event_log
 where TIMESTAMP_X >= now() - interval 7 day
 and AUDIT_TYPE_ID in ('SECURITY_FILTER_PHP','SECURITY_FILTER_XSS','SECURITY_HOST_RESTRICTION','SECURITY_REDIRECT')
 and REMOTE_ADDR IS NOT NULL
-group by AUDIT_TYPE_ID ,REMOTE_ADDR 
-HAVING cn>100
-) as ipt;"`
+group by REMOTE_ADDR
+HAVING cn>5) as ipt ;
+" | tr -s "\n" " "`
 
-iptables -F bx_block; 
-eval $ip_block
+echo "List Block IP"
+echo "-------------------------------------------------------------------------------------------------"
+echo $ip_block
+echo "-------------------------------------------------------------------------------------------------"
+
+fail2ban-client -vvv  set nginx-bad-request banip $ip_block ;
+fail2ban-client -vvv  set sshd banip $ip_block ;
